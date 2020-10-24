db762478f3f1f2aea55c0cf21f34e6bdcae4363e
ajit-t-5144 <64395530+ajit-t-5144@users.noreply.github.com>
2020-10-23 22:15:35 -0700

    new file v1.0

---
+++ jenkinsdocker.sh
@@ -0,0 +1,14 @@
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt install openjdk-8-jdk
+sudo apt-get install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
