75e97f0ca8f6d46f76bc31845c1bc750af0be136
ajit-t-5144 <64395530+ajit-t-5144@users.noreply.github.com>
2020-10-24 19:44:49 -0700

    new dockercompose file

---
+++ dockercompose.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/devopsbc01/Scripts/master/docker-compose.yml
+sudo curl -L "https://github.com/docker/compose/releases/1.27.4/docker-compose --$(uname s)s)--$(uname m)" -o /usr/local/bin/docker compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sysctl -w vm.max_map_count=262144
+docker-compose up -d
