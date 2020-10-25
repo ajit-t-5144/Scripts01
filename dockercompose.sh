ed69b01455669800180d4b8398ec3863c7f36f48
ajit-t-5144 <64395530+ajit-t-5144@users.noreply.github.com>
2020-10-24 19:52:16 -0700

    Update dockercompose.sh

--- dockercompose.sh
+++ dockercompose.sh
@@ -1,5 +1,6 @@
 wget https://raw.githubusercontent.com/devopsbc01/Scripts/master/docker-compose.yml
-sudo curl -L "https://github.com/docker/compose/releases/1.27.4/docker-compose --$(uname s)s)--$(uname m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo sysctl -w vm.max_map_count=262144
+docker-compose --version
 docker-compose up -d
