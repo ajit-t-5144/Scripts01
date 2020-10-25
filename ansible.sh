d8e95c023c3390d49be732cbfb9792650b3dfc72
ajit-t-5144 <64395530+ajit-t-5144@users.noreply.github.com>
2020-10-24 21:31:29 -0700

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -4,4 +4,8 @@ sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 ansible --version
 ansible-galaxy collection install azure.azcollection
-sudo apt-get install python-pip
+sudo apt install python3-pip
+pip3 install packaging
+sudo pip install --upgrade setuptools
+pip3 install azure==4.0.0
+pip3 install azure-storage-queue --upgrade --force-reinstall
