36eac86c67a5ec3a29eaea310ab63ca78593bb8a
ajit-t-5144 <64395530+ajit-t-5144@users.noreply.github.com>
2020-10-26 17:46:38 -0700

    Update ans-tomcat.sh

--- ans-tomcat.sh
+++ ans-tomcat.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade
-wget https://raw.githubusercontent.com/ajit-t-5144/Scripts01/main/ans-tomcat.yml
+wget https://raw.githubusercontent.com/ajit-t-5144/Scripts01/main/ans-tomcat2.yml
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 ansible --version
