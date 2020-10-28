69ad21c0fd72f30bafc4ec9b99d8e20f71d5f4c9
ajit-t-5144 <64395530+ajit-t-5144@users.noreply.github.com>
2020-10-27 17:08:42 -0700

    Create tom-post.sh

---
+++ tom-post.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/devopsbc01/Scripts/master/tomcat.sh
+wget https://raw.githubusercontent.com/devopsbc01/Scripts/master/Postgresql-Install-1.sh
+bash tomcat.sh
+bash Postgresql Install 1.sh
