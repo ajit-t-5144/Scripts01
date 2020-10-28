208e76c8175b53fbf29c316d4a170b0ff2b7f4e9
ajit-t-5144 <64395530+ajit-t-5144@users.noreply.github.com>
2020-10-27 17:16:28 -0700

    Update tom-post.sh

--- tom-post.sh
+++ tom-post.sh
@@ -1,4 +1,6 @@
 wget https://raw.githubusercontent.com/devopsbc01/Scripts/master/tomcat.sh
 wget https://raw.githubusercontent.com/devopsbc01/Scripts/master/Postgresql-Install-1.sh
+sudo apt update
+sudo apt upgrade
 bash tomcat.sh
 bash Postgresql Install 1.sh
