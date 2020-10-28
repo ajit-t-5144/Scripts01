9a24ca865cf96707eb13c2c3e5a1d8330844d945
ajit-t-5144 <64395530+ajit-t-5144@users.noreply.github.com>
2020-10-28 09:34:10 -0700

    Create jenkins-docker-compose.sh

---
+++ jenkins-docker-compose.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/ajit-t-5144/Scripts01/main/jenkinsdocker.sh
+wget https://raw.githubusercontent.com/ajit-t-5144/Scripts01/main/dockercompose.sh
+bash jenkinsdocker.sh
+bash dockercompose.sh
